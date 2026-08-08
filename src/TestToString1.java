28945890afaa6f77e055209c3717986c64933eca
Renjitha V.R <renjithavr18@gmail.com>
2026-08-08 19:26:14 +0530

    TestToString1

---
+++ src/TestToString1.java
@@ -0,0 +1,11 @@
+//toString method
+class Student1{
+    String name="Riya";
+}
+public class TestToString1{
+    public static void main(String[]args){
+        Student1 s=new Student1();
+        System.out.println(s);
+        System.out.println(s.toString());
+
+    }}
